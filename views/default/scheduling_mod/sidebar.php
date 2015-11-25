2c2abdb61d0ae7628a2d426d7e7b55984f0b5b59
Dries <driesdk@gmail.com>
2015-11-25 14:18:22 +0100

    sidebar

--- views/default/scheduling_mod/sidebar.php
+++ views/default/scheduling_mod/sidebar.php
@@ -1,15 +1,12 @@
 <?php
+$owner_guid = elgg_get_logged_in_user_guid();
 $options = array(
 	'type' => 'object',
 	'subtype' => 'scheduling_poll',
 	'full_view' => false,
+    'owner_guid' => $owner_guid,
 );
 
-$container_guid = get_input('container_guid');
-
-if ($container_guid) {
-	$options['container_guid'] = $container_guid;
-}
 
 $content = elgg_list_entities($options);
 echo elgg_view_module('aside', elgg_echo('scheduling:sidebar:title'), $content);
